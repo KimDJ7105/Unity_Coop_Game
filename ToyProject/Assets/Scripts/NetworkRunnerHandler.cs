c9278f67277a8444466697319bc4d6e7c80ebcc3
KimDJ7105 <101383021+KimDJ7105@users.noreply.github.com>
2025-04-16 02:09:59 +0900

    pothon 에셋 설치 및 네트워크 기반 작업

---
+++ ToyProject/Assets/Scripts/NetworkRunnerHandler.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using Fusion;
+using Fusion.Sockets;
+using System.Collections.Generic;
+using System;
+
+public class NetworkRunnerHandler : MonoBehaviour, INetworkRunnerCallbacks
+{
+    public NetworkRunner runnerPrefab;
+    public NetworkRunner runner;
+
+    public string roomName = "GameRoom"; //게임 세션 네임. 중복 불가. 원래 호스트 측에서 입력해야 하지만 지금은 입력 기능이 없으니 사전에 설정
+
+    public async void StartHost()
+    {
+        runner = Instantiate(runnerPrefab);
+        runner.ProvideInput = true;
+
+        await runner.StartGame(new StartGameArgs()
+        {
+            GameMode = GameMode.Host,
+            SessionName = roomName,
+            SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
+        });
+
+        Debug.Log("호스트 생성 완료");
+    }//NetworkManager -> NetworkRunnerHandler.StartHost()
+
+    public async void StartClient()
+    {
+        runner = Instantiate(runnerPrefab);
+        runner.ProvideInput = true;
+
+        await runner.StartGame(new StartGameArgs()
+        {
+            GameMode = GameMode.Client,
+            SessionName = roomName,
+            SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>() //이 코드의 역할과 작동 방식은?
+        });
+
+        Debug.Log("클라이언트 접속 완료");
+    }//NetworkManager -> NetworkRunnerHandler.StartClient()
+
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+        Debug.Log($"플레이어 접속됨 : {player.PlayerId}");
+    }
+
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+    {
+        Debug.Log($"플레이어 퇴장 : {player.PlayerId}");
+    }
+
+    void INetworkRunnerCallbacks.OnConnectedToServer(NetworkRunner runner) { }
+    void INetworkRunnerCallbacks.OnDisconnectedFromServer(Fusion.NetworkRunner runner, Fusion.Sockets.NetDisconnectReason reason) { }
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data) { }
+    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress) { }
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
+    public void OnSceneLoadDone(NetworkRunner runner) { }
+    public void OnSceneLoadStart(NetworkRunner runner) { }
+    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    public void OnInput(NetworkRunner runner, NetworkInput input) { }
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken token) { }
+}
